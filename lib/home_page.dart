792916d74751f18d6b089ec5d842b8ed8c4f9531
Rehan Hamayun <rr8329203@gmail.com>
2022-05-10 16:58:56 +0500

    database fix

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -10,15 +10,49 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  //BOOLEAN
   bool _rehan = false;
+
+  //ARRAYS DATA
+  List<String> productName = [
+    'Mango',
+    'Orange',
+    'Grapes',
+    'Banana',
+    'Chery',
+    'Peach',
+    'Mixed Fruit Basket',
+  ];
+  List<String> productUnit = [
+    'KG',
+    'Dozen',
+    'KG',
+    'Dozen',
+    'KG',
+    'KG',
+    'KG',
+  ];
+  List<int> productPrice = [10, 20, 30, 40, 50, 60, 70];
+  List<String> productImage = [
+    'https://image.shutterstock.com/image-photo/mango-isolated-on-white-background-600w-610892249.jpg',
+    'https://image.shutterstock.com/image-photo/orange-fruit-slices-leaves-isolated-600w-1386912362.jpg',
+    'https://image.shutterstock.com/image-photo/green-grape-leaves-isolated-on-600w-533487490.jpg',
+    'https://media.istockphoto.com/photos/banana-picture-id1184345169?s=612x612',
+    'https://media.istockphoto.com/photos/cherry-trio-with-stem-and-leaf-picture-id157428769?s=612x612',
+    'https://media.istockphoto.com/photos/single-whole-peach-fruit-with-leaf-and-slice-isolated-on-white-picture-id1151868959?s=612x612',
+    'https://media.istockphoto.com/photos/fruit-background-picture-id529664572?s=612x612',
+  ];
+  // ENDS
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         elevation: 0.0,
         backgroundColor: Colors.black87,
         centerTitle: true,
-        title: Text(
+        title: const Text(
           "Products",
           style: TextStyle(
             color: Colors.white,
@@ -35,20 +69,52 @@ class _HomePageState extends State<HomePage> {
               child: Icon(CupertinoIcons.shopping_cart),
             ),
           ),
-          SizedBox(
+          const SizedBox(
             width: 30,
           ),
         ],
       ),
-      body: Container(
-        child: ElevatedButton.icon(
-            onPressed: () {
-              setState(() {
-                _rehan = true;
-              });
-            },
-            icon: Icon(Icons.abc),
-            label: Text("Rehan")),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: productName.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Card(
+                  color: Colors.white,
+                  elevation: 0.0,
+                  child: Row(
+                    children: [
+                      Text(index.toString()),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      Image(
+                        height: 100,
+                        width: 100,
+                        image: NetworkImage(
+                          productImage[index].toString(),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text(
+                        productName[index].toString(),
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 12,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          )
+        ],
       ),
     );
   }
